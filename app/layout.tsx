7291a84f0d0cbfacc729f40f356811ed4fa3cadf
Irene <irenenairuba125@gmail.com>
2026-09-01 13:32:05 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,15 @@
-export default function RootLayout({ children }) {
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Irene's Closet",
+  description: "Online clothing store",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <head>
-        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' rel='stylesheet'/>
-      </head>
-      <body style={{margin:0}}>{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
-  )
+  );
 }
